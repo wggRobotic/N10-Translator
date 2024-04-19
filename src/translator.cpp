e4df1b88c8be84dab0915ae30c4da02ac35f8a1a
SeaCannon1122 <coroc@buzzmail.eu>
2024-04-19 10:59:45 +0200

    renamed node, topics, publishers, subscribers, added drive_enable sub

--- src/translator.cpp
+++ src/translator.cpp
@@ -12,26 +12,31 @@
 class translator : public rclcpp::Node
 {
 public:
-  translator() : Node("translator") {
+  translator() : Node("n10_drive_translator_node") {
     cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("n10/cmd_vel", 10, std::bind(&translator::cmd_vel_callback, this, std::placeholders::_1));
-    ServoOn_sub_ = this->create_subscription<std_msgs::msg::Bool>("ServoOn", 10, std::bind(&translator::ServoOn_callback, this, std::placeholders::_1));
-    servo_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("n10/servo_cmd_vel", 10);
+    drive_enable_sub_ = this->create_subscription<std_msgs::msg::Bool>("n10/drive_enable", 10, std::bind(&translator::drive_enable_callback, this, std::placeholders::_1));
+    servo_enable_sub_ = this->create_subscription<std_msgs::msg::Bool>("n10/servo_enable", 10, std::bind(&translator::servo_enable_callback, this, std::placeholders::_1));
+    servo_cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("n10/servo_cmd_vel", 10);
     //profmay_pub_ = this->create_publisher<??>("??", 10);
     }
 
     void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
-      if(servo_bool) servo_pub_->publish(*msg);
+      if(drive_bool) {
+        if(servo_bool) servo_cmd_vel_pub_->publish(*msg);
+      }
     }
-
-    void ServoOn_callback(const std_msgs::msg::Bool::SharedPtr msg) {servo_bool = msg->data;}
+    void drive_enable_callback(const std_msgs::msg::Bool::SharedPtr msg) {drive_bool = msg->data;}
+    void servo_enable_callback(const std_msgs::msg::Bool::SharedPtr msg) {servo_bool = msg->data;}
     
 
 private:
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr ServoOn_sub_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr servo_pub_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr drive_enable_sub_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr servo_enable_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr servo_cmd_vel_pub_;
   //rclcpp::Publisher<??>::SharedPtr profmay_pub_;
 
+  bool drive_bool = true;
   bool servo_bool = true;
 };
 
