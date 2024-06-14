f2e24337799254b90aa0dc0f508abacad7c87cd4
SeaCannon1122 <coroc@buzzmail.eu>
2024-06-14 16:44:05 +0200

    publishing frequency, angle publisher, servo 2s turning time

--- src/translator.cpp
+++ src/translator.cpp
@@ -14,6 +14,10 @@
 #include "rclcpp/clock.hpp"
 #include "builtin_interfaces/msg/time.hpp"
 
+#define halfwith 0.11f
+#define wheeldistance 0.16f
+#define wheelradius 0.056f
+
 struct vec2f {
   float x;
   float y;
@@ -23,140 +27,146 @@ float mgt(vec2f v) {return sqrt(v.x * v.x + v.y * v.y);}
 
 using namespace std::chrono_literals;
 
-const float halfwith = 0.11;
-const float wheeldistance = 0.16;
-const float wheelradius = 0.056;
+// 0 = left front; 1 = right front; 2 = left middle; 3 = right middle; 4 = left back; 5 = right back;
 
+//translator node
 class translator : public rclcpp::Node {
   public:
     translator() : Node("n10_drive_translator_node") {
+      //subscribers and publishers + clock
       cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("/n10/cmd_vel", 10, std::bind(&translator::cmd_vel_callback, this, std::placeholders::_1));
       servo_enable_sub_ = this->create_subscription<std_msgs::msg::Bool>("/n10/servo_enable", 10, std::bind(&translator::servo_enable_callback, this, std::placeholders::_1));
-      servo_cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/n10/servo_cmd_vel", 10);
+      //servo_cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/n10/servo_cmd_vel", 10);
       motor_vel_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("/n10/motor_vel", 10);
-      angel_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("/n10/servo_cmd_angle", 10);
+      servo_cmd_angel_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("/n10/servo_cmd_angle", 10);
       timer_ = this->create_wall_timer(10ms, std::bind(&translator::timer_callback, this));
       last_call_time_ = this->now();
     }
 
+
     void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
       last_call_time_ = this->now();
-      if(drive_bool) {
-        // 0 = left front; 1 = right front; 2 = left middle; 3 = right middle; 4 = left back; 5 = right back;
-        float ang_vel = msg->angular.z;
-        float lin_x = msg->linear.x;
-        float lin_y = msg->linear.y * M_PI/2;
-        vec2f wheel_vels[6];
 
-        //LOGIC
-        if(servo_bool) {
+      float ang_vel = msg->angular.z * M_PI;
+      float lin_x = msg->linear.x;
+      float lin_y = msg->linear.y;
 
-          wheel_vels[0] = { -halfwith * ang_vel + lin_x, wheeldistance * ang_vel + lin_y};
-          wheel_vels[1] = { halfwith * ang_vel + lin_x, wheeldistance * ang_vel + lin_y};
-          wheel_vels[2] = { -ang_vel * halfwith + lin_x, lin_y};
-          wheel_vels[3] = { ang_vel * halfwith + lin_x, lin_y};
-          wheel_vels[4] = { -halfwith * ang_vel + lin_x, -wheeldistance * ang_vel + lin_y};
-          wheel_vels[5] = { halfwith * ang_vel + lin_x, -wheeldistance * ang_vel + lin_y};
+      vec2f wheel_vels[6];
 
-          //calculate angles
+      // ------ CALCULATION OF SPEEDS AND ANGLES -------//
+      if (servo_bool) {
+        //SPEEDS
+        wheel_vels[0] = { -halfwith * ang_vel + lin_x, wheeldistance * ang_vel + lin_y};
+        wheel_vels[1] = { halfwith * ang_vel + lin_x, wheeldistance * ang_vel + lin_y};
+        wheel_vels[2] = { -ang_vel * halfwith + lin_x, lin_y};
+        wheel_vels[3] = { ang_vel * halfwith + lin_x, lin_y};
+        wheel_vels[4] = { -halfwith * ang_vel + lin_x, -wheeldistance * ang_vel + lin_y};
+        wheel_vels[5] = { halfwith * ang_vel + lin_x, -wheeldistance * ang_vel + lin_y};
 
-          for(int i = 0; i < 6; i++) {
-            if(wheel_vels[i].x == 0) {
-              if(wheel_vels[i].y > 0) angles[i] = M_PI / 2;
-              else if (wheel_vels[i].y < 0) angles[i] = -M_PI / 2;
-              else angles[i] = 0;
-            }
-            else {
-              angles[i] = atan(wheel_vels[i].y / wheel_vels[i].x);
-            }
+        //ANGLES
+        for (int i = 0; i < 6; i++) {
+
+          if (wheel_vels[i].x == 0) {
+
+            if (wheel_vels[i].y > 0) angles[i] = M_PI / 2;
+            else if (wheel_vels[i].y < 0) angles[i] = -M_PI / 2;
+            else angles[i] = 0;
           }
-        }
 
-        else {
-          wheel_vels[0] = { -halfwith * ang_vel + lin_x, 0};
-          wheel_vels[1] = { halfwith * ang_vel + lin_x, 0};
-          wheel_vels[2] = { -ang_vel * halfwith + lin_x, 0};
-          wheel_vels[3] = { ang_vel * halfwith + lin_x, 0};
-          wheel_vels[4] = { -halfwith * ang_vel + lin_x, 0};
-          wheel_vels[5] = { halfwith * ang_vel + lin_x, 0};
-        }
+          else {
+            angles[i] = atan(wheel_vels[i].y / wheel_vels[i].x);
+          }
 
-        //wheel rotations per second and negations based on pointing direction  
-        for(int i = 0; i < 6; i++) {
-          motor_vels[i] = 60 * mgt(wheel_vels[i]) / (2 * wheelradius * M_PI);
-          if(wheel_vels[i].x < 0) motor_vels[i] *= -1; 
         }
-
       }
-    }
-    
-    void timer_callback() {
-        auto vel_msg = std::make_shared<Float32MultiArray>();
-        vel_msg.resize(6);
-        if((this->now() - last_call_time_) / 1e6 < 100 || !servo_currently_moving) {for (int i = 0; i < 6; i++) vel_msg[i] = 0;}
 
-        else {
-        vel_msg[0] = motor_vels[0];
-        vel_msg[1] = motor_vels[1];
-        vel_msg[2] = motor_vels[2];
-        vel_msg[3] = motor_vels[3];
-        vel_msg[4] = motor_vels[4];
-        vel_msg[5] = motor_vels[5];
+      //SPEEDS
+      else {
+        wheel_vels[0] = { -halfwith * ang_vel + lin_x, 0};
+        wheel_vels[1] = { halfwith * ang_vel + lin_x, 0};
+        wheel_vels[2] = { -ang_vel * halfwith + lin_x, 0};
+        wheel_vels[3] = { ang_vel * halfwith + lin_x, 0};
+        wheel_vels[4] = { -halfwith * ang_vel + lin_x, 0};
+        wheel_vels[5] = { halfwith * ang_vel + lin_x, 0};
+      }
+
+      //speeed to wheel rotations per second and negations based on pointing direction  
+      for (int i = 0; i < 6; i++) {
+
+        motor_vels[i] = 60 * mgt(wheel_vels[i]) / (2 * wheelradius * M_PI);
+        if (wheel_vels[i].x < 0) motor_vels[i] *= -1; 
+      }
+
+    }
+
+    //get called every 10ms
+    void timer_callback() {
+        //publish speeds
+        auto vel_msg = std_msgs::msg::Float32MultiArray();
+        vel_msg.data.resize(6);
+
+        if ( (this->now().nanoseconds() - last_call_time_.nanoseconds()) / 1e6 > 100 || servo_currently_moving) {
+          for (int i = 0; i < 6; i++) vel_msg.data[i] = 0;
         }
 
-        motor_vel_pub_->publish(*vel_msg);
+        else { for (int i = 0; i < 6; i++) vel_msg.data[i] = motor_vels[i]; }
 
-        if((angles[0] != last_angles[0] || angles[1] != last_angles[1] || angles[2] != last_angles[2] || angles[3] != last_angles[3] || angles[4] != last_angles[4] || angles[5] != last_angles[5] ) && !servo_currently_moving) {
-          auto angle_msg = std::make_shared<Float32MultiArray>();
-          vel_msg.resize(6);
+        motor_vel_pub_->publish(vel_msg);
 
-          for(int i = 0; i < 6; i++) {
-            angles_new[i] = angles[i];
-            vel_msg[i] = angles[i];
+        //publish angles
+        if ( (angles[0] != last_angles[0] || angles[1] != last_angles[1] || angles[2] != last_angles[2] || angles[3] != last_angles[3] || angles[4] != last_angles[4] || angles[5] != last_angles[5] ) && !servo_currently_moving) {
+
+          auto angle_msg = std_msgs::msg::Float32MultiArray();
+          angle_msg.data.resize(6);
+
+          for (int i = 0; i < 6; i++) {
+            last_angles[i] = angles[i];
+            angle_msg.data[i] = angles[i];
           }
 
-          angel_pub_->publish(*angle_msg);
+          servo_cmd_angel_pub_->publish(angle_msg);
 
           servo_currently_moving = true;
+          servo_started_moving_ = this->now();
         }
+
+        if ((this->now().nanoseconds() - servo_started_moving_.nanoseconds()) / 1e6 > 2000) { servo_currently_moving = false;}
     }
     
     void servo_enable_callback(const std_msgs::msg::Bool::SharedPtr msg) {
-      if(msg->data == false && servo_bool == true) {
-        auto msg = std::make_shared<geometry_msgs::msg::Twist>();
-        msg->linear.x = 1;
-        msg->linear.y = 0;
-        msg->angular.z = 0;
-        servo_cmd_vel_pub_->publish(*msg);
+      if (msg->data == false && servo_bool == true) {
+        for (int i = 0; i < 6; i++) angles[i] = 0;
       }
+
       servo_bool = msg->data;
     }
 
   private:
+    //globals
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr servo_enable_sub_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr servo_cmd_vel_pub_;
+    //rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr servo_cmd_vel_pub_;
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr motor_vel_pub_;
-    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr angel_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr servo_cmd_angel_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
-    
-    //needs to be made thread safe
-    //
-    bool servo_bool = true;
 
-    float angles[6] = {0, 0, 0, 0, 0, 0};
     float motor_vels[6] = {0, 0, 0, 0, 0, 0};
-
-    bool servo_currently_moving = false;
-    
+    float angles[6] = {0, 0, 0, 0, 0, 0};
     float last_angles[6] = {0, 0, 0, 0, 0, 0};
+    
+    bool servo_bool = true;
+    bool servo_currently_moving = false; 
 
     rclcpp::Time last_call_time_;
+    rclcpp::Time servo_started_moving_;
 
 };
 
+
+
+
+
 int main(int argc, char * argv[]) {
-  
   rclcpp::init(argc, argv);
   auto node = std::make_shared<translator>();
   rclcpp::spin(node);
